22e6fb0d74b3a60b0714baad872917d8ab191329
Albert Weinert <info@der-albert.com>
2010-08-02 21:24:51 +0200

    updated build scripts use parameters Compile, Test, Gem, Release

--- default.ps1
+++ default.ps1
@@ -8,14 +8,16 @@ properties {
   $build_dir = "$base_dir\lib" 
   $buildartifacts_dir = "$build_dir\" 
   $sln_file = "$base_dir\Source\FluentMetadata.sln" 
-  $version = "0.5.1.$revision"
+  $version = "0.5.2.$revision"
   $tools_dir = "$base_dir\Tools"
   $release_dir = "$base_dir\Release"
 } 
 
-task default -depends Release
+task default -depends Test
+
 
 task Clean { 
+  FormatTaskName "----------------------------- Executing Task: {0} from FluentMetadata $version"
   remove-item -force -recurse $buildartifacts_dir -ErrorAction SilentlyContinue 
   remove-item -force -recurse $release_dir -ErrorAction SilentlyContinue 
 } 
@@ -41,36 +43,41 @@ task CopyExternals
 }
 task Compile -depends Init { 
   exec { msbuild /t:Rebuild /verbosity:minimal "/p:OutDir=$buildartifacts_dir" "/p:Platform=Any CPU" "$sln_file" }
+  copy-item readme.txt $build_dir\readme.txt
 } 
 
 task Test20 -depends Compile  {
   exec { & $tools_dir\xUnit\xunit.console.exe $build_dir\FluentMetadata.Core.Specs.dll }
-  exec { & $tools_dir\xUnit\xunit.console.exe $build_dir\FluentMetadata.MVC.Specs.dll }
+  exec { & $tools_dir\xUnit\xunit.console.exe $build_dir\FluentMetadata.MVC.Specs.dll }  
 }
 
 task Test40 -depends Test20  {
   exec { & $tools_dir\xUnit\xunit.console.clr4.exe $build_dir\FluentMetadata.EntityFramework.Specs.dll }
 }
 
+task Test -depends Test40
 
-task Docu -depends Test40 {
-   exec { & $lib_dir\xUnit\ReportGenerator.exe /generator:HTML /path:$build_dir /assembly:$build_dir\FluentMetadata.Core.Specs.dll /assembly:$build_dir\FluentMetadata.MVC.Specs.dll }
+task CleanGem -ContinueOnError
+{	
+   Remove-Item .\*.gem
+   exec { gem uninstall fluentmetadata nu -a -x }	
 }
 
-task Gem -depends Compile {
-
-   copy-item readme.txt $build_dir\readme.txt
-   
+task Gem -depends CleanGem 
+{
    $version | out-file .\VERSION -encoding ASCII
    
    exec { gem build .\FluentMetadata.gemspec }
+   exec { gem install fluentmetadata }
 }
-task Release -depends Gem {
+
+task Release -depends Test, Gem {
     
     exec {
     
       & $tools_dir\Zip\zip.exe -9 -A -j `
         $release_dir\FluentMetadata.$version.zip `
+        $build_dir\readme.txt `
         $build_dir\FluentMetadata.Core.dll `
         $build_dir\FluentMetadata.MVC.dll `
         $build_dir\FluentMetadata.FluentNHibernate.dll `
