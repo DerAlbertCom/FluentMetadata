a9f6d6a4cd5909a99bf70d033664cc5f8babe911
Holger Schmidt <leroyz.mailaccount@gmx.net>
2011-04-01 17:35:47 +0200

    initial commit of CopyAutoMappedMetadata feature

---
+++ Source/FluentMetadata.AutoMapper.Specs/SystemUnderTest.cs
@@ -0,0 +1,29 @@
+﻿namespace FluentMetadata.AutoMapper.Specs
+{
+    class Source
+    {
+        public string MyProperty { get; set; }
+    }
+
+    class Destination
+    {
+        public string MyProperty { get; set; }
+    }
+
+    class SourceMetaData : ClassMetadata<Source>
+    {
+        public SourceMetaData()
+        {
+            Class.Display.Name("rzjsfghgafsdfh");
+            Property(x => x.MyProperty).Display.Name("pockänsdfsdf");
+        }
+    }
+
+    class TargetMetaData : ClassMetadata<Destination>
+    {
+        public TargetMetaData()
+        {
+            this.CopyAutoMappedMetadataFrom(typeof(Source));
+        }
+    }
+}
