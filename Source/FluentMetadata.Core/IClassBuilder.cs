74842cb2168247179cfd1f1fdccf5463fd65dc30
Holger Schmidt <leroyz.mailaccount@gmx.net>
2011-04-18 17:30:18 +0200

    made PropertyMustMatchRule available in API throgh Class.PropertiesShouldMatch

--- Source/FluentMetadata.Core/IClassBuilder.cs
+++ Source/FluentMetadata.Core/IClassBuilder.cs
@@ -1,8 +1,15 @@
-﻿namespace FluentMetadata
+﻿using System;
+using System.Linq.Expressions;
+
+namespace FluentMetadata
 {
     public interface IClassBuilder<T>
     {
         Metadata Metadata { get; }
         IDisplayClass<T> Display { get; }
+        IClassBuilder<T> PropertiesShouldMatch(
+            Expression<Func<T, object>> expression,
+            Expression<Func<T, object>> confirmExpression
+        );
     }
 }
